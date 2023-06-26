b9554166c4c90bed7a7f05d78d75f644834af34a
avrahamGol <avragol@gmail.com>
2023-06-26 11:44:41 +0300

    hash and token

--- server/users/routes/userRestController.js
+++ server/users/routes/userRestController.js
@@ -3,10 +3,13 @@ const router = express.Router();
 const handleError = require('../../utils/handleError');
 const userAccessData = require('../models/userAccessData');
 const userValidationService = require('../../validation/userValidationService');
+const hashService = require('../../utils/hash/hashService');
+const tokenService = require('../../utils/token/tokenService');
 
 router.post('/', async (req, res) => {
     try {
         await userValidationService.registerUserValidation(req.body);
+        req.body.password = await hashService.generateHash(req.body.password);
         const dataFromDB = await userAccessData.registerUser(req.body);
         res.json(dataFromDB);
     } catch (err) {
@@ -19,10 +22,15 @@ router.post('/login', async (req, res) => {
         await userValidationService.loginUserValidation(req.body);
         let { email, password } = req.body;
         let dataFromDB = await userAccessData.getUserByEmail(email);
-        if (dataFromDB && dataFromDB.password === password) {
-            res.json({ msg: "done!", dataFromDB });
+        if (!dataFromDB || !(await hashService.cmpHash(password, dataFromDB.password))) {
+            throw new Error("Invaild email or password");
         } else {
-            handleError(res, "Invaild email or password", 400)
+            let token = await tokenService.generateToken({
+                isManger: dataFromDB.isManager,
+                groupId: dataFromDB.groupId,
+                _id: dataFromDB._id
+            });
+            res.json({ msg: "done!", token });
         }
     } catch (err) {
         handleError(res, err.message, 404)
