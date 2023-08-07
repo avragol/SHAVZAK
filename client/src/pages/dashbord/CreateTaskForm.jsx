a874274f16f31d4259cb38c0485b34dfacda0c95
avrahamGol <avragol@gmail.com>
2023-08-07 15:34:28 +0300

    validation create task form

--- client/src/pages/dashbord/CreateTaskForm.jsx
+++ client/src/pages/dashbord/CreateTaskForm.jsx
@@ -3,18 +3,24 @@ import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import axios from 'axios';
 
+import { feildValidation } from '../../validation/feildValidation';
+import { createTaskSchema, RequierdRoleSchema } from '../../validation/tasks/createTask';
+
 const CreateTaskForm = () => {
 
     const [formData, setFormData] = useState({
         name: '',
         groupId: '',
-        requierdRoles: [],
-        rangeTimeStart: '',
-        rangeTimeEnd: '',
+        requierdRoles: [{ title: '', amount: '' },],
+        rangeTime: ['', ''],
     });
-    const [roles, setRoles] = useState([{ title: '', amount: '' }]);
+    const [errors, setErrors] = useState({
+        name: '',
+        requierdRoles: [{ title: '', amount: '' },],
+        rangeTime: ['', ''],
+    });
     const payload = useSelector((bigState) => bigState.auth.payload);
-    const [errors, setErrors] = useState({});
+
     const navigate = useNavigate();
     useEffect(() => {
         fetchGroup();
@@ -26,26 +32,40 @@ const CreateTaskForm = () => {
         }
     }
 
-    const handleChange = (e) => {
+    const handleChange = e => {
         const { name, value } = e.target;
         setFormData(prev => ({ ...prev, [name]: value }));
+
+        const error = feildValidation(createTaskSchema[name], value, name);
+        setErrors(prev => ({ ...prev, [name]: error }));
     }
 
     const addRole = () => {
-        setRoles(prev => [...prev, { title: '', amount: '' }])
+        const roles = [...formData.requierdRoles, { title: '', amount: '' }];
+        setFormData(prev => ({ ...prev, requierdRoles: roles }));
+
+        const errorRoles = [...errors.requierdRoles, { title: '', amount: '' }];
+        setErrors(prev => ({ ...prev, requierdRoles: errorRoles }));
     }
     const removeRole = () => {
-        setRoles(prev => {
-            const updatedRoles = [...prev];
-            updatedRoles.pop();
-            return updatedRoles;
-        });
+
+        let roles = [...formData.requierdRoles];
+        roles.pop();
+        setFormData(prev => ({ ...prev, requierdRoles: roles }));
+
+        let errorRoles = [...errors.requierdRoles];
+        errorRoles.pop();
+        setErrors(prev => ({ ...prev, requierdRoles: errorRoles }));
     }
 
     const handleRoleChange = (index, field, value) => {
-        const updatedRoles = [...roles];
+        const updatedRoles = [...formData.requierdRoles];
         updatedRoles[index][field] = value;
-        setRoles(updatedRoles);
+        setFormData(prev => ({ ...prev, requierdRoles: updatedRoles }));
+        const error = feildValidation(RequierdRoleSchema[field], value, field);
+        const updatedErrorsRoles = [...errors.requierdRoles]
+        updatedErrorsRoles[index][field] = error
+        setErrors(prev => ({ ...prev, requierdRoles: updatedErrorsRoles }))
     }
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -59,7 +79,7 @@ const CreateTaskForm = () => {
     }
 
     return (
-        <div className="bg-white p-10 rounded-lg shadow-md">
+        <div className="bg-gray-100 dark:bg-gray-700 p-10 rounded-lg shadow-md">
             <h2 className="text-lg font-medium mb-4">Create New Task</h2>
 
             <form onSubmit={handleSubmit}>
@@ -73,26 +93,30 @@ const CreateTaskForm = () => {
                         value={formData.name}
                         onChange={handleChange}
                     />
+                    <span className='text-red-600'>{errors.name}</span>
                 </div>
                 <div className="mb-4">
-                    {roles.map((role, index) => (
+                    {formData.requierdRoles.map((role, index) => (
                         <div key={index} className="flex mb-4">
-
-                            <input
-                                className="border p-2 rounded w-full mr-2"
-                                placeholder="Title"
-                                value={role.title}
-                                onChange={e => handleRoleChange(index, 'title', e.target.value)}
-                            />
-
-                            <input
-                                className="border p-2 rounded w-full ml-2"
-                                placeholder="Amount"
-                                type="number"
-                                value={role.amount}
-                                onChange={e => handleRoleChange(index, 'amount', e.target.value)}
-                            />
-
+                            <div>
+                                <input
+                                    className="border p-2 rounded w-full mr-2"
+                                    placeholder="Title"
+                                    value={role.title}
+                                    onChange={e => handleRoleChange(index, 'title', e.target.value)}
+                                />
+                                <span className='text-red-600 w-100'>{errors.requierdRoles[index].title}</span>
+                            </div>
+                            <div>
+                                <input
+                                    className="border p-2 rounded w-full ml-2"
+                                    placeholder="Amount"
+                                    type="number"
+                                    value={role.amount}
+                                    onChange={e => handleRoleChange(index, 'amount', e.target.value)}
+                                />
+                                <span className='text-red-600'>{errors.requierdRoles[index].amount}</span>
+                            </div>
                         </div>
                     ))}
 
@@ -103,7 +127,7 @@ const CreateTaskForm = () => {
                     >
                         Add Role
                     </button>
-                    {roles && roles.length > 1 &&
+                    {formData.requierdRoles.length > 1 &&
                         <button
                             className="bg-blue-500 text-white p-2 rounded ml-2"
                             onClick={removeRole}
@@ -120,22 +144,22 @@ const CreateTaskForm = () => {
                         className="border p-2 w-full rounded"
                         type="datetime-local"
                         name="rangeTimeStart"
-                        value={formData.rangeTimeStart}
+                        value={formData.rangeTime[0]}
                         onChange={handleChange}
                     />
                 </div>
-
+                <span className='text-red-600'>{errors.rangeTime[0]}</span>
                 <div className="mb-4">
                     <label className="block font-medium mb-2">End Date</label>
                     <input
                         className="border p-2 w-full rounded"
                         type="datetime-local"
                         name="rangeTimeEnd"
-                        value={formData.rangeTimeEnd}
+                        value={formData.rangeTime[1]}
                         onChange={handleChange}
                     />
+                    <span className='text-red-600'>{errors.rangeTime[1]}</span>
                 </div>
-
                 <button
                     className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
                     type="submit">
--- client/src/validation/tasks/createTask.js
+++ client/src/validation/tasks/createTask.js
@@ -1,13 +1,18 @@
-import Joi from 'joi';
+import Joi from "joi";
 
-const CreateTaskSchema = {
-    name: Joi.string().min(2).max(256).required(),
-    email: Joi.string().email({ tlds: { allow: false } }).required(),
-    password: Joi.string().pattern(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/)
-        .messages({ 'string.pattern.base': `Password must contain at least one lowercase letter, one uppercase letter, one number, and one special character from @$!%*?&.` }),
-    groupId: Joi.string().optional(),
-    roles: Joi.array().items(Joi.string()).optional(),
+const RequierdRoleSchema = {
+    title: Joi.string().trim().min(2).max(256).required(),
+    amount: Joi.number().min(1).required(),
+};
+
+const createTaskSchema = {
+    name: Joi.string().trim().min(2).max(256).required(),
+    groupId: Joi.string().regex(/^[0-9a-fA-F]{24}$/),
+    rangeTime: Joi.array()
+        .items(Joi.date().min('now'))
+        .length(2)
+        .required(),
 };
 
 
-export default CreateTaskSchema;
+export { createTaskSchema, RequierdRoleSchema };
